9f9dd3d3a8d77cb26d4168cf63afee9d09a0b5b3
Farhan <itsfarhanashraf@gmail.com>
2025-11-04 03:21:35 +0500

    fix allowed hosts for railway

--- academy/settings.py
+++ academy/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-l*i#h$*8@lonb(vqd@x#a1d$*_9@m-ho1fdzk+fzz(rjrfw!#=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.up.railway.app',   # allows ANY subdomain on Railway
+]
 
 
 # Application definition
